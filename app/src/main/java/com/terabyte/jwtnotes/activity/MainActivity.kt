d30fad825c3a1467029bf3133ab610df70ed93d6
artem <art.holodilov@gmail.com>
2026-04-17 19:56:19 +0300

    Basic navigation component

--- app/src/main/java/com/terabyte/jwtnotes/activity/MainActivity.kt
+++ app/src/main/java/com/terabyte/jwtnotes/activity/MainActivity.kt
@@ -4,6 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.terabyte.jwtnotes.ui.navigation.Route
+import com.terabyte.jwtnotes.ui.screen.CreateNoteScreen
+import com.terabyte.jwtnotes.ui.screen.ListNotesScreen
+import com.terabyte.jwtnotes.ui.screen.LoginScreen
+import com.terabyte.jwtnotes.ui.screen.RegistrationScreen
+import com.terabyte.jwtnotes.ui.screen.UpdateNoteScreen
 import com.terabyte.jwtnotes.ui.theme.JwtNotesTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,7 +24,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JwtNotesTheme {
+                val navController = rememberNavController()
 
+                NavHost(
+                    navController = navController,
+                    startDestination = Route.LoginRoute.route
+                ) {
+
+                    composable(Route.LoginRoute.route) {
+                        LoginScreen()
+                    }
+
+                    composable(Route.RegistrationRoute.route) {
+                        RegistrationScreen()
+                    }
+
+                    composable(Route.ListNotesRoute.route) {
+                        ListNotesScreen()
+                    }
+
+                    composable(Route.CreateNoteRoute.route) {
+                        CreateNoteScreen()
+                    }
+
+                    composable(Route.UpdateNoteRoute.route) {
+                        UpdateNoteScreen()
+                    }
+                }
             }
         }
     }
---
+++ app/src/main/java/com/terabyte/jwtnotes/viewmodel/LoginViewModel.kt
@@ -0,0 +1,11 @@
+package com.terabyte.jwtnotes.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class LoginViewModel @Inject constructor() : ViewModel() {
+
+}
