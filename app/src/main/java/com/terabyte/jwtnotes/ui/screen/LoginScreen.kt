deb6f0c1f7a19417044bf9c8d3aff014002a7c1a
artem <art.holodilov@gmail.com>
2026-04-17 20:47:19 +0300

    Basic UI of LoginScreen

--- app/src/main/java/com/terabyte/jwtnotes/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/terabyte/jwtnotes/ui/screen/LoginScreen.kt
@@ -3,10 +3,15 @@ package com.terabyte.jwtnotes.ui.screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,12 +20,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.terabyte.jwtnotes.R
 import com.terabyte.jwtnotes.ui.theme.JwtNotesTheme
 import com.terabyte.jwtnotes.viewmodel.LoginViewModel
 
@@ -42,8 +50,8 @@ fun LoginScreen(
         topBar = {
             Text(
                 text = "Log in to your account",
-                fontSize = 18.sp,
-                color = Color.Black,
+                fontSize = 22.sp,
+                color = MaterialTheme.colorScheme.onPrimary,
                 textAlign = TextAlign.Center,
                 modifier = Modifier
                     .fillMaxWidth()
@@ -60,6 +68,80 @@ fun LoginScreen(
                 .padding(paddingValues)
                 .padding(16.dp)
         ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = "Username:",
+                    fontSize = 16.sp,
+                    color = Color.Black
+                )
+                OutlinedTextField(
+                    value = "",
+                    singleLine = true,
+                    onValueChange = {
+
+                    },
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 8.dp)
+                )
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            ) {
+                Text(
+                    text = "Password:",
+                    fontSize = 16.sp,
+                    color = Color.Black
+                )
+                OutlinedTextField(
+                    value = "",
+                    singleLine = true,
+                    onValueChange = {
+
+                    },
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 8.dp)
+                )
+                IconButton(
+                    onClick = {
+
+                    }
+                ) {
+                    Icon(
+                        painterResource(R.drawable.ic_password_invisible),
+                        contentDescription = null,
+                        tint = Color.Black
+                    )
+                }
+            }
+
+            Button(
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .padding(top = 32.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.login)
+                )
+            }
+
+            Text(
+                text = stringResource(R.string.or_create_new_account),
+                color = Color.Blue,
+                modifier = Modifier
+                    .padding(top = 32.dp)
+            )
 
         }
     }
